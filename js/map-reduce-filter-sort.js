acab3bb8bcf93e8fcc1762ce905444b5bdea83bf
Silver Ringvee <silversillu@gmail.com>
2019-03-04 13:00:09 +0200

    0 --> -1

--- js/map-reduce-filter-sort.js
+++ js/map-reduce-filter-sort.js
@@ -13,8 +13,8 @@ const output = {
 output.tops = arr.filter(v => v > 1000); // Filter in only the values over 1000
 output.sum = arr.reduce((acc, cur) => acc + cur); // Get the sum of all values
 output.withTax = arr.map(v => v * 1.1); // Add tax to all values
-output.asc_1 = [...arr].sort((a, b) => (a > b ? 1 : 0)); // Sort values low to high
-output.desc_1 = [...arr].sort((a, b) => (a < b ? 1 : 0)); // Sort values high to low
+output.asc_1 = [...arr].sort((a, b) => (a > b ? 1 : -1)); // Sort values low to high
+output.desc_1 = [...arr].sort((a, b) => (a < b ? 1 : -1)); // Sort values high to low
 output.asc_2 = [...arr].sort((a, b) => a - b); // Sort values low to high
 output.desc_2 = [...arr].sort((a, b) => b - a); // Sort values high to low
 
